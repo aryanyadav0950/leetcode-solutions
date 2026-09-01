a5319b7ffea2c732630eaec4609748d76c8c9c22
Aryan Yadav <aryanyadav0950@gmail.com>
2026-09-01 09:53:38 +0530

    Time: 83 ms (20.74%), Space: 113.5 MB (15.95%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -5,9 +5,9 @@ public:
         for(int x:nums){
             mp[x]++;
         }
-        for(auto x : mp){
-            cout<<x.first<<" "<<x.second<<endl;
-        }
+        // for(auto x : mp){
+        //     cout<<x.first<<" "<<x.second<<endl;
+        // }
         for(int x : nums){
             if(mp[x] >= 2){
                 return true;
