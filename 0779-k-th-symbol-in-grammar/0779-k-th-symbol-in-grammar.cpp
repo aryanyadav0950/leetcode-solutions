f2e43f894bc451072ca81dab773114de7fcc9275
Aryan Yadav <aryanyadav0950@gmail.com>
2026-09-12 22:51:37 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.72%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int kthGrammar(int n, int k) {
+        if(n==1) return 0;
+        if(k%2==0){
+            int prevAns=kthGrammar(n-1,k/2);
+            if(prevAns==0) return 1;
+            else return 0;
+        }
+        else{
+            int prevAns=kthGrammar(n-1,k/2+1);
+            return prevAns;
+        }
+
+        
+    }
+};
